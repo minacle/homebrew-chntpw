c330e580b67054675c7825bc981cab4fcec2b48d
Mina Her <minacle@live.com>
2024-06-13 04:14:56 +0900

    chntpw 1.0.3

--- Formula/chntpw.rb
+++ Formula/chntpw.rb
@@ -1,8 +1,8 @@
 class Chntpw < Formula
   desc "The Offline NT Password Editor"
   homepage "https://github.com/minacle/chntpw"
-  url "https://github.com/minacle/chntpw/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "51625a7b7c67120830669d309cb5f11f0ec1815a753cbf33e28715f8a8befff1"
+  url "https://github.com/minacle/chntpw/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "e2febc050ccd97d26318c1a00f9303377566e8f461b0c1903fd4757f3a2b37b5"
   head "https://github.com/minacle/chntpw.git", branch: "main"
   license "GPL-2.0-only"
 
@@ -14,6 +14,6 @@ class Chntpw < Formula
   end
 
   test do
-    assert_match "chntpw 1.0.2,", shell_output("#{bin}/chntpw -h")
+    assert_match "chntpw 1.0.3,", shell_output("#{bin}/chntpw -h")
   end
 end
